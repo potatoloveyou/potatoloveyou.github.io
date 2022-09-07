f7ccff0ccc647f823c33be0c021ae81da787ed53
Your Name <2444265157@qq.com>
2022-09-07 10:06:16 +0800

    css添加

---
+++ index.js
@@ -0,0 +1,22 @@
+// let skills=[
+//     'html5',
+//     'css3',
+//     'javascript',
+//     'jquery',
+//     'ajax',
+//     'vue',
+// ]
+// $(skills).each(function (i,e) {
+//     // console.log(e);
+//     let node =document.createElement('a');
+//     $(node).addClass(e).html(e);
+
+//     // $(node);
+//     $('.turntable').append(node)
+// })
+
+
+// skills.forEach(function (e) {
+//     let node =document.createElement('div');
+//     node.
+// })
